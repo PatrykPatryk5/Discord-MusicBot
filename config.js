f85e30d95ad82a96e118cb9ccdd0a55436e89572
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2024-07-25 07:04:17 +0000

    modified:   dashboard/out/_next/static/chunks/pages/index-0494ad302e38da35.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: "719878365640916993", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTI2NTYyNDk4NzY3NTUyNTE5MQ.GbxFs7.aYFhsoy8Sg6kOtz9DvZfYlAskrLPqDtgow90Sw", //- Bot's Token
+	clientId: process.env.clientId || "1265624987675525191", //- ID of the bot
+	clientSecret: process.env.clientSecret || "HdTwYVmQVGiYELrvA7HBV27zuc-sSqtg", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
